236ad8357afdcda6ea9ae9ece3c8203dd78840ae
DEEPESH GODWANI <deepeshgodwani28@gmail.com>
2022-11-24 22:55:27 +0530

    tested auth api

---
+++ models/chat-box.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+
+const chatBoxSchema=mongoose.Schema({
+ 
+    chatname:{
+        type:String,
+        trim:true
+    },
+    isGroupChat:{type:Boolean,default:false},
+    users:[
+        {
+            userId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"user"
+            }
+        }
+    ],
+    admin:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+    latestMessage:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'message'
+    }
+},{
+    timesstamps:true,
+})
+
+
+const chatBox=mongoose.model("chat",chatBoxSchema);
+module.exports=chatBox;
